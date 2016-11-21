6f7ace5a90a293abd0c16aba1644eae33a7d31d2
Matheus Martins <matheusdealmeidamartins@gmail.com>
2016-11-21 21:54:03 -0200

    pushing login items

--- login.php
+++ login.php
@@ -1,3 +1,22 @@
+<?php
+
+$page_title = "Log in";
+
+require_once "header.php";
+
+?>
+<div class="col-xs-12 col-md-4 text-center">
+	<h1>Flora Marcos Ono</h1>
+	<form action="verify-login.php" method="POST">
+		<label for="username">Usuário: </label>
+		<input type="text" name="username" placeholder="Ex: marcos">
+		<br>
+		<label for="password">Senha: </label>
+		<input type="password" name="password" placeholder="Sua senha de acesso">
+		<br>
+		<input type="submit">
+	</form>
+</div>
 <h1>Welcome to your login page!</h1>
 <form action="verify-login.php" method="POST" class="login-form">
 	<input type="text" name="username" placeholder="Username">
