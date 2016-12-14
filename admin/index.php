98f8ceaddf999cc58f64c57f71096fc4624e0611
Rtadashiono <ricardotadashiono@gmail.com>
2016-12-14 09:18:44 -0200

    Fixing the Admin side. Already has Session

--- admin/index.php
+++ admin/index.php
@@ -1,9 +1,15 @@
 <?php
 
-session_start();
+session_start("log");
+
+if ($_SESSION['log'] != "ativo") {
+    session_destroy();
+    header("location:login.php");
+}
 
 if ( isset( $_SESSION[ 'id' ] ) && ! empty( $_SESSION[ 'id' ] ) ) {
 	echo "Area Restrita";
+    echo '<br>'. $_SESSION['nome'];
 } else {
 	header( 'Location: login.php' );
 }
